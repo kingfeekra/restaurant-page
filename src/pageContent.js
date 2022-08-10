c317d2e9dcdb13ea77f0b1111409006117be675d
kingfeekra <fiachraking@gmail.com>
2022-08-10 14:59:40 +0100

    add opening hours and information sections

--- src/pageContent.js
+++ src/pageContent.js
@@ -33,6 +33,41 @@ const pageLoad = function() {
     introPara.classList.add("introPara");
     introDivSelect.appendChild(introPara);
 
+    const hoursDiv = document.createElement("div");
+    hoursDiv.classList.add("hoursDiv");
+    contentDivSelect.appendChild(hoursDiv);
+
+    const hoursDivSelect = document.querySelector(".hoursDiv");
+    const hoursHeading = document.createElement("h2");
+    hoursHeading.textContent = "Opening Hours";
+    hoursDivSelect.appendChild(hoursHeading);
+
+    const hoursList = document.createElement("ul");
+    for(let i = 0; i < 7; i++) {
+        const day = document.createElement("li");
+        hoursList.appendChild(day);
+    }
+    hoursDivSelect.appendChild(hoursList);
+
+    const listItems = document.querySelectorAll("li");
+    listItems[0].textContent = "Monday: 10am-9pm";
+    listItems[1].textContent = "Tuesday: 10am-9pm";
+    listItems[2].textContent = "Wednesday: 10am-9pm";
+    listItems[3].textContent = "Thursday: 10am-9pm";
+    listItems[4].textContent = "Friday: 10am-11pm";
+    listItems[5].textContent = "Saturday: 10am-11pm";
+    listItems[6].textContent = "Sunday: 10am-10pm";
+
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("infoDiv");
+    contentDivSelect.appendChild(infoDiv);
+
+    const infoDivSelect = document.querySelector(".infoDiv");
+    const infoHeading = document.createElement("h2");
+    infoHeading.textContent = "Contact Info";
+    infoDivSelect.appendChild(infoHeading);
+
+
 }
 
 
